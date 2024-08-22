eea7d136ee8442ed03e015099a203ce03b25339e
Gerard Joseph <gerarz7536@gmail.com>
2024-08-22 18:44:49 +0200

    edgarr23.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/edgarr23.py
@@ -0,0 +1,24 @@
+#Sitio oficial del lenguaje: https://es.python.org#
+
+#Una linea de comentario#
+
+"""
+Varias lineas de comentario
+1
+2
+3
+"""
+
+var = 0
+
+CONSTANTE = 0
+
+string = ""
+entero = 2
+booleano = False
+booleano = True
+lista = []
+flotante = 1.5
+
+print("Hola Python!")
+
